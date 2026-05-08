5094cd821ad331cb7cbabc153cdf81acd0220bdf
Benjamin N. Zahirpour <156618785+Bzahirpour@users.noreply.github.com>
2026-05-08 12:29:55 -0400

    added front end (#4)
    * added front end
    
    * fixed provider block
    
    ---------
    
    Co-authored-by: Bzahirpour <Bzahirpour@users.noreply.github.com>

--- infra/envs/prod/outputs.tf
+++ infra/envs/prod/outputs.tf
@@ -1,4 +1,14 @@
-output "pipeline_test_parameter" {
-  description = "Name of the validation parameter"
-  value       = aws_ssm_parameter.pipeline_test.name
+output "website_url" {
+  description = "CloudFront distribution URL"
+  value       = module.static_site.website_url
+}
+
+output "bucket_name" {
+  description = "S3 bucket name for frontend file sync"
+  value       = module.static_site.bucket_name
+}
+
+output "distribution_id" {
+  description = "CloudFront distribution ID for cache invalidation"
+  value       = module.static_site.distribution_id
 }
---
+++ infra/modules/static-site/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  description = "Project name used in resource naming"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (dev or prod)"
+  type        = string
+}
